0734d7be9ebb03984c970ee0928a6d6e2d18675e
Christopher Gosser and Michael Harmon <christopher@>
2018-08-09 11:37:07 -0700

    id number buttons and make them append field

---
+++ js/scripts.js
@@ -0,0 +1,55 @@
+$(document).ready(function() {
+  // edit this work later
+  // $("#formOne").submit(function() {
+  //   $(".person1").append("blah blah");
+  //   $(".person2").append("blah blah");
+  //   $(".animal").append("blah blah");
+  //   $(".exclamation").append("blah blah");
+  //   $(".verb").append("blah blah");
+  //   $(".noun").append("blah blah");
+  //
+  //   $("#story").show();
+  // });
+
+$("#one").click(function(){
+    var numInput = $( "#numInput" );
+numInput.val( numInput.val() + "1" );
+})
+$("#two").click(function(){
+    var numInput = $( "#numInput" );
+numInput.val( numInput.val() + "2" );
+})
+$("#three").click(function(){
+    var numInput = $( "#numInput" );
+numInput.val( numInput.val() + "3" );
+})
+$("#four").click(function(){
+    var numInput = $( "#numInput" );
+numInput.val( numInput.val() + "4" );
+})
+$("#five").click(function(){
+    var numInput = $( "#numInput" );
+numInput.val( numInput.val() + "5" );
+})
+$("#six").click(function(){
+    var numInput = $( "#numInput" );
+numInput.val( numInput.val() + "6" );
+})
+$("#seven").click(function(){
+    var numInput = $( "#numInput" );
+numInput.val( numInput.val() + "7" );
+})
+$("#eight").click(function(){
+    var numInput = $( "#numInput" );
+numInput.val( numInput.val() + "8" );
+})
+$("#nine").click(function(){
+    var numInput = $( "#numInput" );
+numInput.val( numInput.val() + "9" );
+})
+$("#zero").click(function(){
+    var numInput = $( "#numInput" );
+numInput.val( numInput.val() + "0" );
+})
+
+});
